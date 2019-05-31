683a03777feedef468496e4e074fdc43fbcdec57
Nykolay <savin171099@gmail.com>
2019-05-31 15:09:58 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     <Header/>
 
-      </header>
     </div>
   );
 }
