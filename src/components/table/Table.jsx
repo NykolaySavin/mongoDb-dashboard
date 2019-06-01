e8a14253a607c11af5554e730112259b50313d02
Nykolay <savin171099@gmail.com>
2019-06-01 20:46:22 +0300

    Change link style

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 export default function Table({ items, title,deleteItem }) {
   return (
@@ -14,7 +15,7 @@ export default function Table({ items, title,deleteItem }) {
           {items.map(item => (
             <tr key={item["_id"]}>
               <td>{item.title}</td>
-              <td style={{width:150}}><a>Edit</a> | <a onClick={()=>deleteItem(item["_id"])}>Delete</a></td>
+              <td  style={{width:150}}><NavLink>Edit</NavLink> | <NavLink onClick={()=>deleteItem(item["_id"])}>Delete</NavLink></td>
             </tr>
           ))}
         </tbody>
