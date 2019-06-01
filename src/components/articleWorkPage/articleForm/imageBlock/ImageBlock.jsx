33fb21e2081fd5ec536b7fb3b9432d17e5ee2296
Nykolay <savin171099@gmail.com>
2019-06-01 19:49:42 +0300

    Add hook for image showing

--- src/components/articleWorkPage/articleForm/imageBlock/ImageBlock.jsx
+++ src/components/articleWorkPage/articleForm/imageBlock/ImageBlock.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React,{useEffect,useState}  from 'react';
 import FormTextInput from '../../../form/formTextInput/FormTextInput.container';
 import FormFileInput from "../../../form/formFileInput/FormFileInput.container";
 
 export default function ImageBlock({item,workingItem}) {
     const img = workingItem[`${item}.image.0`];
+    const [link,getLink]=useState('');
+        useEffect(()=>{
+            if(img){
+                const reader = new FileReader();
+                reader.onloadend=()=>getLink(reader.result);
+                reader.readAsDataURL(img);
+            }
+
+        })
     return(<div className="image-block">
-        <img src={img?img:null}/>
+        <img  src={link?link:null}/>
         <div className="form-group">
             <FormTextInput id={`${item}.image-caption`} placeholder="Image Caption"/>
             <FormFileInput id={`${item}.image.0`} label="Select File"/>
